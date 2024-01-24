dda97ded1c8b4e513ef6a401aad7f30fd0daba85
Samuel Henao <codinsamueldev@gmail.com>
2024-01-24 17:47:00 -0500

    new: code example of linear time complexity algorithm

---
+++ ztm_master_coding_interview_course.py/big_o_notation/linear_time.py
@@ -0,0 +1,13 @@
+def linear_example(arr):
+    """
+    This function demonstrates linear time complexity O(n).
+    It prints each element of the given array.
+
+    :param arr: List or array
+    """
+    for element in arr:
+        print(element)
+
+# Example usage:
+my_array = [1, 2, 3, 4, 5]
+linear_example(my_array)
