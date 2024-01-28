32565261f824d5e712953ee516f3af4654450f4f
Samuel Henao <codinsamueldev@gmail.com>
2024-01-28 00:19:11 -0500

    fix: typo + problem description

--- ztm_master_coding_interview_course.py/arrays/exercises/reverse_string.py
+++ ztm_master_coding_interview_course.py/arrays/exercises/reverse_string.py
@@ -1,9 +1,21 @@
+"""
+Create a function which takes a string as parameter, and return the string reversed.
+
+Example 1:
+Input -> Samuel
+Output -> leumaS
+
+Example 2:
+Input -> I am a millonaire
+Output -> erianollim a ma I
+"""
+
 
 def reverse_string(word: str) -> str: # O(1).
     return word[::-1]
     
 
-def reverse_string(word: str) -> str: # 0(n).
+def reverse_string2(word: str) -> str: # 0(n).
     
     solution = str()
     length = len(word) - 1
@@ -18,5 +30,5 @@ def reverse_string(word: str) -> str: # 0(n).
 if __name__ == "__main__":
     # print(reverse_string("Samuel"))
 
-    print(reverse_string2("Samuel"))
+    print(reverse_string2("I am a millonaire"))
 
