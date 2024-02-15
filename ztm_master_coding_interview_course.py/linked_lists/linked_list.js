a975de7197839beb17c3b83ea1fedb4e76703115
Samuel Henao <codinsamueldev@gmail.com>
2024-02-15 16:39:20 -0500

    feat: create node class, create insert method to add data to a given index, finally create method to visualize linked list

--- ztm_master_coding_interview_course.py/linked_lists/linked_list.js
+++ ztm_master_coding_interview_course.py/linked_lists/linked_list.js
@@ -1,3 +1,11 @@
+class Node {
+    constructor (data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+
 class LinkedList {
     constructor (data) {
         this.head = {
@@ -10,39 +18,75 @@ class LinkedList {
 
     prepend (data) {
         /* Add nodes to linked list head. */
-        if (this.head == null) {
-            let addNode = data
-            this.head.data = addNode
+        if (this.head === null) {
+            let addNode = new Node(data)
+            this.head = addNode
         }
         else {
-            const newNode = {
-                data: data,
-                next: this.head
-            }
+            const newNode = new Node(data)
+            newNode.next = this.head
             this.head = newNode
         }
     }
 
 
-    append_nodes (data) {
-        if (this.head == null) {
+    appendNodes (data) {
+        /* Insert data at linked list's tail. Time complexity O(1). */
+        if (this.head === null) {
             this.prepend(data)
         }
         else {
-            const newNode = {
-                data: data,
-                next: null
-            }
+            const newNode = new Node(data)
             this.tail.next = newNode
             this.tail = newNode
         }
     }
+
+
+    insertNodeAt (index, data) {
+        /* Insert new data at index given. Time complexity O(n). */
+
+        if (index < 0) {
+            throw new Error('Invalid index...')
+        } else if (index === 0) {
+            this.prepend(data)
+        }
+
+        let count = 0
+        let iterator = this.head
+
+        while (iterator !== null) {
+            if (count === index - 1) {
+                const newNode = new Node(data) // Create new node.
+                newNode.next = iterator.next // Set the new node's next pointer to the next node of the current node.
+                iterator.next = newNode // Set the current node's next pointer to the new node.
+                return
+            }
+            count++
+            iterator = iterator.next
+        }
+    }
+
+
+    printOutLinkedList () {
+        let iterator = this.head
+        const llArray = [];
+
+        while (iterator !== null) {
+            llArray.push(iterator.data)
+            iterator = iterator.next
+        }
+        return llArray
+    }
 }
 
 
+// Example usage.
 const MyLinkedList = new LinkedList(10)
 
 MyLinkedList.prepend(5)
-MyLinkedList.append_nodes(15)
+MyLinkedList.prepend(115)
+MyLinkedList.appendNodes(15)
+MyLinkedList.insertNodeAt(2, "Holi")
 
-console.log(MyLinkedList)
+console.log(MyLinkedList.printOutLinkedList())
